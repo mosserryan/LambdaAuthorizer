938e1fa4d94d29a280a9f6949bebcb3a771fd6c4
mosserryan <mosser.ryan.a@gmail.com>
2023-03-13 15:24:38 -0700

    Initial setup of Authorizer, testing functionality with hardcoded values

--- LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/authorizer/LambdaAuthorizer.java
+++ LambdaAuthorizer/src/main/java/com/appsdeveloperblog/aws/lambda/authorizer/LambdaAuthorizer.java
@@ -3,17 +3,51 @@ package com.appsdeveloperblog.aws.lambda.authorizer;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
-import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+import java.util.Arrays;
 
 /**
  * Handler for requests to Lambda function.
  */
-public class LambdaAuthorizer implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+public class LambdaAuthorizer implements RequestHandler<APIGatewayProxyRequestEvent, AuthorizerOutput> {
 
     @Override
-    public APIGatewayProxyResponseEvent handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
+    public AuthorizerOutput handleRequest(final APIGatewayProxyRequestEvent input, final Context context) {
 
-        return new APIGatewayProxyResponseEvent();
+        String userName = input.getPathParameters().get("userName");
+        String effect = "Allow";
+
+        if (userName.equalsIgnoreCase("123")) {
+            effect = "Deny";
+        }
+
+        APIGatewayProxyRequestEvent.ProxyRequestContext proxyRequestContext = input.getRequestContext();
+
+        String arn = String.format("arn:aws:execute-api:%s:%s:%s/%s/%s/%s",
+                System.getenv("AWS_REGION"),
+                proxyRequestContext.getAccountId(),
+                proxyRequestContext.getApiId(),
+                proxyRequestContext.getStage(),
+                proxyRequestContext.getHttpMethod(),
+                "*");
+
+        Statement statement = Statement.builder()
+                .action("execute-api:Invoke")
+                .effect(effect)
+                .resource(arn)
+                .build();
+
+        PolicyDocument policyDocument = PolicyDocument.builder()
+                .version("2012-10-17")
+                .statements(Arrays.asList(statement))
+                .build();
+
+        AuthorizerOutput authorizerOutput = AuthorizerOutput.builder()
+                .principalId(userName)
+                .policyDocument(policyDocument)
+                .build();
+
+        return authorizerOutput;
     }
 
 }
